d4408632e873114f3d7456f477a3038bb1c8acd9
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-08-29 20:47:12 -0400

    - Changed cohorts/applicants data type from obj to array so data is easier to access - Cohorts controller now querying by cohort type using url query, will be used in home page cohort type selection

--- src/routes/cohorts/cohorts.controller.js
+++ src/routes/cohorts/cohorts.controller.js
@@ -1,27 +1,34 @@
-const db = require('../../db')
+const db = require('../../db');
+
+
+const cohortsRef = db.collection("cohorts");
 
 const getCohortsController = (req, res) => {
+    const cohortType = req.query.cohortType
 
-    db.collection("cohorts")
+    cohortsRef
+    .where('cohortType', '==', cohortType)
     .get()
     .then(snapshot => {
+
         res.json({
             data: snapshot.docs.map(doc => {
                 return {
                     id: doc.id,
+                    numApplicants: doc.data().applicants.length,
                     ...doc.data()
                 };
             })
         });
-        // console.log("TESTING RES!");
-        // console.log(res);
     })
+
     .catch(error => {
         res.json({ error });
     });
 
   };
   
+
   module.exports = {
     getCohortsController
   }
