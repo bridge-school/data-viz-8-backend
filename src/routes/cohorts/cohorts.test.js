5d278d27e5f9c5fbb5743aee79828345e12de986
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-09-04 08:18:29 -0400

    Re-wrote a cohort router test to use dummy data. Made note to return to this test file later to add more test cases.

--- src/routes/cohorts/cohorts.test.js
+++ src/routes/cohorts/cohorts.test.js
@@ -1,9 +1,31 @@
 const request = require("supertest");
-
+const express = require('express');
 const { cohortsRouter } = require("./cohorts.router");
 
-describe("tests for the cohorts endpoint", () => {
-  it("returns a 200", () => {
-    expect(true).toBe(true);
+const app = express();
+
+app.get('/cohorts?type=filter', (req, res) =>{
+  res.status(200).json(
+    {"cohorts": {
+      "cohort1": {
+        "type": "filter"
+      },
+      "cohort2": {
+        "type": "filter"
+      },
+      "cohort3": {
+        "type": "filter out"
+      }
+    }
+  })
+})
+
+// TODO: return to this to add more test cases
+describe("GET/ cohorts by type", () => {
+  it("responds with json", () => {
+    request(app)
+      .get('/cohorts?type=filter')
+      .expect('Content-Type', /json/)
+      .expect(200)
   });
 });
