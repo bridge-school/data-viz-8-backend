089f3e23fc9b1f67ae992fb105be7d1d00b29476
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-09-04 08:30:16 -0400

    Tried reformatting test server code; test passing locally.

--- src/routes/cohorts/cohorts.test.js
+++ src/routes/cohorts/cohorts.test.js
@@ -2,28 +2,32 @@ const request = require("supertest");
 const express = require('express');
 const { cohortsRouter } = require("./cohorts.router");
 
-const app = express();
-
-app.get('/cohorts?type=filter', (req, res) =>{
-  res.status(200).json(
-    {"cohorts": {
-      "cohort1": {
-        "type": "filter"
-      },
-      "cohort2": {
-        "type": "filter"
-      },
-      "cohort3": {
-        "type": "filter out"
-      }
-    }
-  })
-})
 
 // TODO: return to this to add more test cases
 describe("GET/ cohorts by type", () => {
+
+  const app = express();
+
+  app.get('/cohorts?type=filter', (req, res) =>{
+    res.status(200).json(
+      {"cohorts": {
+        "cohort1": {
+          "type": "filter"
+        },
+        "cohort2": {
+          "type": "filter"
+        },
+        "cohort3": {
+          "type": "filter out"
+        }
+      }
+    })
+  })
+
+  const agent = request.agent(app);
+
   it("responds with json", () => {
-    request(app)
+    agent
       .get('/cohorts?type=filter')
       .expect('Content-Type', /json/)
       .expect(200)
