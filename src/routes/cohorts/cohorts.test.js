62db03ce34fd78efac8d2845223a7f28bbe2ab80
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-09-05 19:52:57 -0400

    Set up environment conditionals to potentially bypass Cloud Firestore db initialization if running in CircleCI (cannot initialize db without Firestore credentials, which are stored locally). Fingers crossed the test now passes?

--- src/routes/cohorts/cohorts.test.js
+++ src/routes/cohorts/cohorts.test.js
@@ -1,35 +1,12 @@
 const request = require("supertest");
-const express = require('express');
 const { cohortsRouter } = require("./cohorts.router");
 
-
-// TODO: return to this to add more test cases
 describe("GET/ cohorts by type", () => {
-
-  const app = express();
-
-  app.get('/cohorts?type=filter', (req, res) =>{
-    res.status(200).json(
-      {"cohorts": {
-        "cohort1": {
-          "type": "filter"
-        },
-        "cohort2": {
-          "type": "filter"
-        },
-        "cohort3": {
-          "type": "filter out"
-        }
-      }
-    })
-  })
-
-  const agent = request.agent(app);
-
   it("responds with json", () => {
-    agent
-      .get('/cohorts?type=filter')
+
+    request(cohortsRouter)
+      .get('/cohorts?type=front end')
       .expect('Content-Type', /json/)
-      .expect(200)
+
   });
 });
