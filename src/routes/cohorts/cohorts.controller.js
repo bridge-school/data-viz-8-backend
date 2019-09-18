2965f5c8ab2e40b33dfcef64b3c178d166a22cee
Gaby Küpfer <33757394+gabycodes@users.noreply.github.com>
2019-09-18 14:41:39 -0400

    New route for getting cohort by cohort number (#2)

--- src/routes/cohorts/cohorts.controller.js
+++ src/routes/cohorts/cohorts.controller.js
@@ -4,6 +4,8 @@ const cohortsRef = db.collection("cohorts");
 
 // capture cohorts by type (i.e. front end, design)
 const getCohortsTypeController = (req, res) => {
+    // http://localhost:8081/cohorts/cohort?cohortType=front%20end
+
     // capturing url params
     const cohortType = req.query.cohortType;
 
@@ -12,11 +14,17 @@ const getCohortsTypeController = (req, res) => {
     .get()
     .then(snapshot => {
         res.json({
-            data: snapshot.docs.map(doc => {
-                return {
-                    id: doc.id,
-                    ...doc.data()
-                };
+            data: snapshot.docs
+                .filter(doc => {
+                    console.log(doc.id);
+                    // return (`cohort-${cohortNumber}` === doc.id);
+                    return doc
+                })
+                .map(doc => {
+                    return {
+                        id: doc.id,
+                        ...doc.data()
+                    };
             })
         });
     })
@@ -27,7 +35,41 @@ const getCohortsTypeController = (req, res) => {
     });
   };
   
+  // capture cohorts by number, within a type (i.e. cohort 8 of the front end cohorts)
+  const getCohortController = (req, res) => {
+    // http://localhost:8081/cohorts/cohort?cohortType=front%20end&cohortNumber=cohortNumberGoesHere
+
+    const cohortType = req.query.cohortType;
+    const cohortNumber = req.query.cohortNumber;
+
+    cohortsRef
+        .where('cohortType', '==', cohortType)
+        .get()
+        .then(snapshot => {
+            res.json({
+                data: snapshot.docs
+                    .filter(doc => {
+                        console.log(doc.id);
+                        return (`cohort-${cohortNumber}` === doc.id);
+                    })
+                    .map(doc => {
+                        console.log("anything");
+                        return {
+                            id: doc.id,
+                            ...doc.data()
+                        };
+                    })
+            });
+        })
+
+        .catch(error => {
+            console.log("ERROR " + error)
+            res.json({ error });
+        });
+};
+
   module.exports = {
-    getCohortsTypeController
+    getCohortsTypeController,
+    getCohortController,
   }
   
