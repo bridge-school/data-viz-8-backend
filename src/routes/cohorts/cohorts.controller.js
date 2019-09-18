e6700e753c5edca107cd389c677f0b86f09ba739
Farzana Mostafa <farzana.mostafa.f@gmail.com>
2019-09-17 20:59:12 -0400

    filter cohort by cohort number

--- src/routes/cohorts/cohorts.controller.js
+++ src/routes/cohorts/cohorts.controller.js
@@ -6,28 +6,36 @@ const cohortsRef = db.collection("cohorts");
 const getCohortsTypeController = (req, res) => {
     // capturing url params
     const cohortType = req.query.cohortType;
+    const cohortNumber = req.query.cohortNumber;
+
+    console.log(cohortNumber);
 
     cohortsRef
-    .where('cohortType', '==', cohortType)
-    .get()
-    .then(snapshot => {
-        res.json({
-            data: snapshot.docs.map(doc => {
-                return {
-                    id: doc.id,
-                    ...doc.data()
-                };
-            })
-        });
-    })
+        .where('cohortType', '==', cohortType)
+        .get()
+        .then(snapshot => {
+            res.json({
+                data: snapshot.docs
+                    .filter(doc => {
+                        console.log(doc.id);
+                        return (`cohort-${cohortNumber}` === doc.id);
+                    })
+                    .map(doc => {
+                        console.log("anything");
+                        return {
+                            id: doc.id,
+                            ...doc.data()
+                        };
+                    })
+            });
+        })
 
-    .catch(error => {
-        console.log("ERROR " + error)
-        res.json({ error });
-    });
-  };
-  
-  module.exports = {
+        .catch(error => {
+            console.log("ERROR " + error)
+            res.json({ error });
+        });
+};
+
+module.exports = {
     getCohortsTypeController
-  }
-  
+}
